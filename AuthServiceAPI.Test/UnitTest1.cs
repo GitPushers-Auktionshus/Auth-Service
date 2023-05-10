ab40fbcc786db84105e72a1b88f1bb82d1ccf3d4
Anders <90785699+lynge96@users.noreply.github.com>
2023-05-10 13:15:29 +0200

    Fil struktur oprettet + Dockerfile.

---
+++ AuthServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AuthServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
