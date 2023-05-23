8c8d13a44efea72c7716d14887d6edb01ec233cd
Jacob Kaae <jacobkaae0511@gmail.com>
2023-05-23 20:48:33 +0200

    Bugfixes

--- AuthServiceAPI/Program.cs
+++ AuthServiceAPI/Program.cs
@@ -48,7 +48,7 @@ try
     IVaultClient vaultClient = new VaultClient(vaultClientSettings);
 
     // Uses vault client to read key-value secrets.
-    Secret<SecretData> enviromentVariables = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: "enviromentVariables", mountPoint: "secret");
+    Secret<SecretData> enviromentVariables = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: "environmentVariables", mountPoint: "secret");
     Secret<SecretData> connectionString = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: "connectionStrings", mountPoint: "secret");
 
     // Initialized string variables to store enviroment secrets
